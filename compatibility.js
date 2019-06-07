407c3c03cea2ef0fb8461bf6d3fab3daddd92b9e
Heath Morrison <heath@doublemarked.com>
2019-06-07 11:29:26 +0200

    Expanded compatibility interface

--- compatibility.js
+++ compatibility.js
@@ -4,9 +4,19 @@ import browsers from './compatible-browsers.json'
 const DEFAULT_UA = typeof window !== 'undefined' ? window.navigator.userAgent : undefined
 
 export const isSupportedBrowser = (ua = DEFAULT_UA) => bowser.check(browsers.supported, true, ua)
+export const isSupportedBrowserVendor = (ua = DEFAULT_UA) => Object.keys(browsers.supported).some(k => bowser[k])
 export const isLatestBrowser = (ua = DEFAULT_UA) => bowser.check(browsers.latest, true, ua)
 
 export default {
+  _bowser: bowser,
+  _browsers: browsers,
+
   isSupportedBrowser,
+  isSupportedBrowserVendor,
   isLatestBrowser,
+
+  isAndroid: bowser.android,
+  isIOS: bowser.ios,
+  isMac: bowser.mac,
+  isWindows: bowser.windows,
 }
