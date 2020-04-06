26218b84d2a5142b2eb7526b4787e248f2832c8b
nickmartyn <kolia@doxy.me>
2020-04-06 17:35:59 +0300

    CS-341 hasMicrophone and hasCamera fix

--- index.js
+++ index.js
@@ -8,26 +8,36 @@ let localStream = null;
 
 /** @implements {MediaAccess} */
 class DoxymeMediaAccess {
-  constructor() {}
+  constructor() {
+    this.userMediaStatus = {};	
+    waitForDeviceInfo().then(userMediaStatus => {	
+      this.userMediaStatus = userMediaStatus;	
+    });	
+  }
 
   requestMediaAccess() {
     if(localStream) return Promise.resolve(localStream);
-    // return waitForDeviceInfo().then(deviceInfo => {
-      return navigator.mediaDevices.getUserMedia({
-        audio: true,
-        video: true
-      }).then(stream => {
-          const speech = hark(stream, {
-            interval: 500
-          });
-          speech.on('volume_change', volume => {
-            emitter.emit('localVolumeChange', volume);
-          });
-          localStream = stream;
-          return stream;
-        });
-  }
+      return waitForDeviceInfo().then(() => {
+        return navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: true
+        }).then(stream => {
+          return waitForDeviceInfo().then(userMediaStatus => {
+            this.userMediaStatus = userMediaStatus;
 
+            const speech = hark(stream, {
+              interval: 500
+            });
+            speech.on('volume_change', volume => {
+              emitter.emit('localVolumeChange', volume);
+            });
+            localStream = stream;
+            return stream;
+          });
+        });
+    });
+  }
+  
   on(...args) {
     return emitter.on(...args)
   }
