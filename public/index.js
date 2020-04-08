5b464b228ec6ff3215498cae5776be49a2cad19d
nickmartyn <kolia@doxy.me>
2020-04-08 18:43:09 +0300

    CS-341 hasCamera and Safari enumerateDevices

--- public/index.js
+++ public/index.js
@@ -3,12 +3,17 @@ import DoxyMe from '../'
 
 console.log(DoxyMe);
 
-DoxyMe.requestMediaAccess().then(stream => {
-  console.log(stream);
-  console.log(DoxyMe);
-}).catch(err => {
-  console.error(err);
-});
+let videoEl = document.querySelector('video');
+let button = document.querySelector('.startVideo')
+
+button.addEventListener('click', () => {
+  DoxyMe.requestMediaAccess().then(stream => {
+    console.log(stream);
+    videoEl.srcObject = stream
+  }).catch(err => {
+    console.error(err);
+  });
+})
 
 DoxyMe.on('localVolumeChange', volume => {
   console.log(volume);
